3f9949b458a10d144a16984f3857dd6a9aa7cab2
Adlet <pioneermove@gmail.com>
2024-10-15 14:16:30 +0600

    feat: added database configuration and cors filters

---
+++ src/main/java/result/server/messengerbackend/wire/postgresql/infrastructure/DatabaseConfiguration.java
@@ -0,0 +1,15 @@
+package result.server.messengerbackend.wire.postgresql.infrastructure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"result.server.messengerbackend"})
+@EnableJpaAuditing
+public class DatabaseConfiguration {
+
+}
---
+++ src/main/java/result/server/messengerbackend/wire/scheduling/infrastructure/SchedulingConfiguration.java
@@ -0,0 +1,8 @@
+package result.server.messengerbackend.wire.scheduling.infrastructure;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+public class SchedulingConfiguration {
+
+}
